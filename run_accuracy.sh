25e6ca6d3e43f364dbd6041767ef7b6b71fbc0cd
ziqizh <zqiiii@hotmail.com>
2019-10-29 22:24:04 -0400

    Modify script

--- run_accuracy.sh
+++ run_accuracy.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-path="/data/hzzheng/Code.baseline-atta.cifar10_challenge.10.21/data-model/"
+#path="/data/hzzheng/Code.baseline-atta.cifar10_challenge.10.21/data-model/"
 #python measure-converge.py --model-name m.1.model --ckpt-end 79000
 #python draw_converge.py --model-name m.1.model
 #python measure-converge.py --model-name m.1.model.nlr
 #python draw_converge.py --model-name m.1.model.nlr
-python measure-converge.py --model-name m.3.model --ckpt-end 79000 --model-dir $path --gpuid 3
-python draw_converge.py --model-name m.3.model
-python measure-converge.py --model-name m.5.model --ckpt-end 32000 --ckpt-step 2000 --model-dir $path --gpuid 3
-python draw_converge.py --model-name m.5.model
-python measure-converge.py --model-name m.7.model --ckpt-end 15000 --ckpt-step 1000 --model-dir $path --gpuid 3
-python draw_converge.py --model-name m.7.model
-python measure-converge.py --model-name m.10.model
-python draw_converge.py --model-name m.10.model
+#python measure-converge.py --model-name m.3.model --ckpt-end 79000 --gpuid 3
+#python draw_converge.py --model-name m.3.model
+python measure-converge.py --model-name m.5.model --ckpt-end 32000 --ckpt-step 2000 --model-dir /data/hzzheng/Code.baseline-atta.cifar10_challenge.10.21/data-model/ --gpuid 3
+#python draw_converge.py --model-name m.5.model
+python measure-converge.py --model-name m.7.model --ckpt-end 15000 --ckpt-step 1000 --model-dir /data/hzzheng/Code.baseline-atta.cifar10_challenge.10.21/data-model/ --gpuid 3
+#python draw_converge.py --model-name m.7.model
+python measure-converge.py --model-name m.10.model --gpuid 3 --model-dir /data/hzzheng/Code.baseline-atta.cifar10_challenge.10.21/data-model/
+#python draw_converge.py --model-name m.10.model
 #python measure-converge.py --model-name m.10.model.nlr
 #python draw_converge.py --model-name m.10.model.nlr
 
